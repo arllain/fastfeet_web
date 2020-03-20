effa9e6a36177449c97caf1cd2eccb9a7646ff66
arllainm <arllain@gmail.com>
2020-03-20 12:16:42 -0300

    Added custom css for CustomButton

--- src/components/CustomButton/styles.js
+++ src/components/CustomButton/styles.js
@@ -2,23 +2,30 @@ import styled, { css } from 'styled-components';
 import { darken } from 'polished';
 import colors from '~/styles/colors';
 
-const addStyles = css`
+const defaultStyles = css`
   background: ${colors.purple} 0 0 no-repeat padding-box;
   color: ${colors.light};
+  width: ${props => props.width};
+  height: ${props => props.heigh};
+
+  /* width: 142px;
+  height: 36px; */
   transition: background 0.2s;
   &:hover {
     background: ${darken(0.03, '#7d40e7')};
   }
 `;
 
-const defaultStyles = css`
-  background-color: ${colors.gray};
-  color: white;
+const backButtonStyles = css`
+  width: ${props => props.width};
+  height: ${props => props.heigh};
+  background-color: ${colors.gray_cc};
+  color: ${colors.light};
 `;
 
 const getButtonStyles = props => {
-  if (props.isAddButton) {
-    return addStyles;
+  if (props.isBackButton) {
+    return backButtonStyles;
   }
   return defaultStyles;
 };
@@ -26,8 +33,6 @@ const getButtonStyles = props => {
 export const Container = styled.button`
   display: flex;
   justify-content: center;
-  width: 142px;
-  height: 36px;
   opacity: 1;
 
   border: 0;
